c8ea37749a07232d1277ba4fe2a6516b8255d3b1
xiaopeng-pyhon <wuxiaoyanhenni@163.com>
2018-12-27 15:13:02 +0800

    201812271512

--- booktest/models.py
+++ booktest/models.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# encoding:utf-8
 from django.db import models
 
 # Create your models here.
+
+class BookInfo(models.Model):
+    btile = models.CharField(max_length=30)
+    bpub_data = models.DateField()

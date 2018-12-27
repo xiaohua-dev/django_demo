bbae03312b958437bdeaf8842bf43b1997ca72b4
xiaopeng-pyhon <wuxiaoyanhenni@163.com>
2018-12-27 16:11:16 +0800

    201812271611

--- booktest/models.py
+++ booktest/models.py
@@ -4,6 +4,15 @@ from django.db import models
 
 # Create your models here.
 
+#一类
 class BookInfo(models.Model):
     btile = models.CharField(max_length=30)
     bpub_data = models.DateField()
+
+
+#多类
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=20)
+    hgender = models.BooleanField(default=False)
+    hgender = models.CharField(max_length=248)
+    hbook = models.ForeignKey('BookInfo')#两表之间建立关系

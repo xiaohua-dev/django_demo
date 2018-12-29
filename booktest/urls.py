66e1fc09fa87e00229128a80cfbb599006b0ea64
xiaopeng-pyhon <wuxiaoyanhenni@163.com>
2018-12-29 10:45:56 +0800

    201812291045

--- booktest/urls.py
+++ booktest/urls.py
@@ -3,6 +3,6 @@ from booktest import views
 
 urlpatterns = [
     #通过url函数设置url路由函数
-    url(r'^index', views.index), #建立/index和视图index之间的关系
-    url(r'^index2', views.index2),
+    url(r'^index/$', views.index), #建立/index和视图index之间的关系
+    url(r'^index2/$', views.index2),
 ]

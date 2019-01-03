4f90c9333b081b791f478edb8478ad1ed6b00a64
xiaopeng-pyhon <wuxiaoyanhenni@163.com>
2019-01-03 15:16:25 +0800

    201901031516

--- firstsite/view.py
+++ firstsite/view.py
@@ -3,4 +3,5 @@ import os
 
 def hello(request):
     command_status = os.popen('cat /space/aa.txt'.encode("utf-8")).read()
-    return HttpResponse("%s".decode() %(command_status))
+    result = command_status.decode
+    return HttpResponse("%s" %(result))

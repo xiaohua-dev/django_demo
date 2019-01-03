35c62ceaa22a500e3f7dc2496b2456cbd44dd322
xiaopeng-pyhon <wuxiaoyanhenni@163.com>
2019-01-03 15:19:15 +0800

    201901031519

--- firstsite/view.py
+++ firstsite/view.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python
+# encoding:utf-8
 from django.http import HttpResponse
 import os
 
 def hello(request):
-    command_status = os.popen('ifconfig eth0'.encode("utf-8")).read()
+    command_status = os.popen('ifconfig eth0').read()
     result = command_status.decode
     return HttpResponse("%s" %(result))

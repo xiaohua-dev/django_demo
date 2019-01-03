8bd40b7ee10349aa6b98c5321f2eacef8c0ee619
xiaopeng-pyhon <wuxiaoyanhenni@163.com>
2019-01-03 15:17:50 +0800

    201901031517

--- firstsite/view.py
+++ firstsite/view.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 import os
 
 def hello(request):
-    command_status = os.popen('cat /space/aa.txt'.encode("utf-8")).read()
+    command_status = os.popen('ifconfig eth0'.encode("utf-8")).read()
     result = command_status.decode
     return HttpResponse("%s" %(result))
